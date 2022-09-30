df6a7e1f6ccf4455d4c71dbf8133cf8b0bcf1739
New User <New User@DESKTOP-AJAC63U>
2022-09-30 17:25:46 +0530

    commit first

--- src/main/java/com/houarizegai/calculator/Calculator.java
+++ src/main/java/com/houarizegai/calculator/Calculator.java
@@ -29,7 +29,7 @@ public class Calculator {
     private char opt = ' '; // Save the operator
     private boolean go = true; // For calculate with Opt != (=)
     private boolean addWrite = true; // Connect numbers in display
-    private double val = 0; // Save the value typed for calculation;
+    private double val = 0; // Save the value typed for calculatinn
 
     /*
         Mx Calculator: 
